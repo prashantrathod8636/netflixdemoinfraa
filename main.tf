f5a7ce1370a18884fb0eaac78cd6d8ec642c40cd
prashantrathod8636 <mrprashantrathod8636@gmail.com>
2025-01-31 20:20:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "pacific-server"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-043863a98cb6091ad"]
 tags = {
 Name = var.instance_names[count.index]
 }
